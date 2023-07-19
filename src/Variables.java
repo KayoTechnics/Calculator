8f30a221f1384f9be8cc73862f003311ad306871
KayoTechnics <martmakkormic@gmail.com>
2023-07-20 02:56:33 +0300

    first commit

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 153;
+        short myShort = 32111;
+        long myLong = 100000000;
+        double myDouble = 235.12;
+        float myFloat = 234.43214f;
+        char myChar = 'a';
+        boolean b = true;
+        byte myByte = 100; //-66-96
+
+
+
+        System.out.println(myChar);
+    }
+}
